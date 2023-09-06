b88274a629447d8df2fbdeeb9384f98700fcd2cb
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-09-06 10:49:46 -0300

    Repositórios iniciados - UsuarioRepository pronto

---
+++ senai.inlock.webApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+using senai.inlock.webApi.Domains;
+using senai.inlock.webApi.Interfaces;
+
+namespace senai.inlock.webApi.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private string StringConexao = "Data Source = NOTE04-S15; Initial Catalog = inlock_games_manha; User Id = sa; Pwd = Senai@134";
+
+        public UsuarioDomain Login(string email, string password)
+        {
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string queryLogin = "SELECT IdUsuario,Email,IdTipoUsuario FROM Usuario WHERE Email = @email AND Senha = @password";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryLogin, con))
+                {
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    if (rdr.Read())
+                    {
+                        UsuarioDomain usuario = new UsuarioDomain()
+                        {
+                            IdUsuario = Convert.ToInt32(rdr["IdUsuario"]),
+                            Email = rdr["Email"].ToString(),
+                            IdTipoUsuario = Convert.ToInt32(rdr["IdTipoUsuario"])
+                        };
+                        return usuario;
+                    }
+
+                    return null;
+                }
+            }
+        }
+    }
+}
